87153c1d3863d896acc55a3a1bd39611cebe7811
KAC CHAN <110216345+KAC-CHAN@users.noreply.github.com>
2022-09-29 16:25:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5702183929:AAH2_oIzX98z-DWMl_zrCf2MX8jstzL01_I")
     
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 18791409))
     
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "389c73d03dc1e98d973a125065d67b3d")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -24,10 +24,10 @@ class Config(object):
     
     PROCESS_MAX_TIMEOUT = 3600
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5163444566"))
 
-    SESSION_NAME = "UploadLinkToFileBot"
+    SESSION_NAME = "AnicadeUploaderBot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://kacbusiness:hrituzee123@kacbusiness.dahr62x.mongodb.net/?retryWrites=true&w=majority")
 
     MAX_RESULTS = "50"
